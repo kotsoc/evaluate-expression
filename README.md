3587624c6cefc1808a4cf096ec2255a8321aefdd
Konstantinos Peratinos <kotsosgr@gmail.com>
2026-02-28 20:53:03 +0100

    Multiplication/division done

---
+++ README.md
@@ -0,0 +1,27 @@
+# Mathematical expression Evaluator
+A simple program that evaluates a string consiting of numbers and mathematical operators.
+- Support the 4 basic operators: Addition (+), Subtraction (-), Multiplication (*), and Division (/).
+- Support both positive and negative integers.
+
+## Usage
+
+## Assumptions
+- Length of the expression should between 1 and 2147483647
+- Expression has valid form, the code only check for valid start,end or empty expresisons
+
+Task:
+Create a function that calculates and returns the value of an integer expression given as
+a space-separated string.
+
+Examples
+EX1:
+calculate("2 + 3")
+EX2:
+calculate("3 * 2 + 1")
+EX3:
+calculate("3 * -2 + 6")
+
+Requirements
+• Support the 4 basic operators:
+Addition (+), Subtraction (-), Multiplication (*), and Division (/).
+• Support both positive and negative integers.
---
+++ src/lib/ExpressionEvaluator.java
@@ -0,0 +1,124 @@
+package lib;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExpressionEvaluator {
+    
+    //Nested class to hold the operand value and its position in the expression
+    class ArithmeticOperand {
+        BigDecimal value;
+        int startIndex;
+        int endIndex;
+
+        public ArithmeticOperand(BigDecimal value, int startIndex, int endIndex) {
+            this.value = value;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
+        }
+    }
+
+    private Set<Character> operators = new HashSet<>(Arrays.asList('+', '-', '*', '/'));
+
+    // This method assumes the input is a valid mathematical expression in string
+    // format.
+    // In case of invalid or no input, it will return BigDecimal.ZERO
+    public String evaluate(String expression) {
+        if (isInValidExpression(expression)) {
+            return "";
+        }
+        return multipleDivide(expression);
+    }
+    //7543
+    private ArithmeticOperand findLeftOperand(String expression, int i) {
+        //Sanity check to ensure we don't go out of bounds
+        if(i < 0) {
+           throw new ArithmeticException("No left operand found for the operator at index " + (i+2));
+        }
+
+        BigDecimal leftOperand = BigDecimal.ZERO;
+        int dec = 0;
+        int endIndex = i;
+        while (i >= 0 && expression.charAt(i) != ' ') {
+            // Found the sign
+            if (expression.charAt(i) == '-') {
+                leftOperand = leftOperand.multiply(new BigDecimal(-1));
+                break;
+            } else if (expression.charAt(i) == '+') {
+                break;
+            } else {
+                leftOperand = leftOperand.add(new BigDecimal(Character.getNumericValue(expression.charAt(i))).multiply(new BigDecimal(10).pow(dec)));
+                dec++;
+            }
+            i--;
+        }
+        return new ArithmeticOperand(leftOperand, Math.max(i, 0), endIndex);
+    }
+
+    private ArithmeticOperand findRightOperand(String expression, int i) {
+        //Sanity check to ensure we don't go out of bounds
+        if(i >= expression.length()) {
+           throw new ArithmeticException("No right operand found for the operator at index " + (i-2));
+        }
+        int startIndex = i;
+        BigDecimal rightOperand = BigDecimal.ZERO;
+        BigDecimal sign = BigDecimal.ONE;
+        if (expression.charAt(i) == '-') {
+            sign = BigDecimal.valueOf(-1);
+            i++;
+        }
+
+        while (i < expression.length() && expression.charAt(i) != ' ') {
+            rightOperand = rightOperand.multiply(new BigDecimal(10))
+                    .add(new BigDecimal(Character.getNumericValue(expression.charAt(i))));
+            i++;
+        }
+        return new ArithmeticOperand(rightOperand.multiply(sign), startIndex, Math.min(i, expression.length()-1));
+    }
+
+    // This method should check if the input expression is valid.
+    // It will check for invalid starting/ending characters and null or empty
+    // expression.
+    // A complete check is out of scope for this example.
+    boolean isInValidExpression(String expression) {
+        if (expression == null || expression.isEmpty() || expression.startsWith("*") || expression.endsWith("*")
+                || expression.startsWith("/") || expression.endsWith("/")) {
+            return true;
+        }
+        return false;
+    }
+
+    String multipleDivide(String expression) {
+        BigDecimal result = BigDecimal.ZERO;
+        ArithmeticOperand leftOperand, rightOperand;
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '*' || expression.charAt(i) == '/') {
+                leftOperand = findLeftOperand(expression, i-2);
+                rightOperand = findRightOperand(expression, i+2);
+                if(expression.charAt(i) == '*') {
+                    result = leftOperand.value.multiply(rightOperand.value);
+                } else {
+                    if(rightOperand.value.compareTo(BigDecimal.ZERO) == 0) {
+                       throw new ArithmeticException("Division by zero is not allowed");
+                    }
+                    result = leftOperand.value.divide(rightOperand.value, 10, RoundingMode.HALF_UP);
+                }
+                //Take from the start to the space before the left operand, add the result and then add the rest of the expression after the right operand
+                System.out.println("First substring '" +expression.substring(0, leftOperand.startIndex)+"'");
+                System.out.println("Second substring '" +expression.substring(rightOperand.endIndex + 1)+"'");
+                expression = expression.substring(0, leftOperand.startIndex) +" "+ result.toPlainString() + " " + expression.substring(rightOperand.endIndex + 1);
+                i = leftOperand.startIndex + result.toPlainString().length() - 1; //It is ok to skip the space after the operand 
+                System.out.println("Intermediate expression after evaluating operator at index " + i + ": " + expression);
+            }
+        }
+        return expression;
+    }
+}
+
+// overflows
+// valid expression
+// testing
+ 
